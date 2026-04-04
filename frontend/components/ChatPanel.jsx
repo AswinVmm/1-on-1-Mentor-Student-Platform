989bf3a1cc872524712e12c561a94272c61b848c
AswinVmm <aswina002@gmail.com>
2026-04-04 17:56:42 +0530

    chat

--- frontend/components/ChatPanel.jsx
+++ frontend/components/ChatPanel.jsx
@@ -3,16 +3,19 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
 export default function ChatPanel({ sessionId }) {
+    const [myId, setMyId] = useState(null);
     const [messages, setMessages] = useState([]);
     const [input, setInput] = useState("");
     const [socket, setSocket] = useState(null);
-    const myId = typeof window !== "undefined"
-        ? localStorage.getItem("userId")
-        : null;
+    // const myId = typeof window !== "undefined"
+    //     ? localStorage.getItem("userId")
+    //     : null;
 
     useEffect(() => {
         const token = localStorage.getItem("token");
-        const myId = localStorage.getItem("userId");
+        const userId = localStorage.getItem("userId");
+
+        setMyId(userId);
 
         const newSocket = io(process.env.NEXT_PUBLIC_BASE_URL, {
             auth: { token },
@@ -35,9 +38,9 @@ export default function ChatPanel({ sessionId }) {
         });
 
         return () => {
-            newSocket.off("chat-history");
-            newSocket.off("receive-message");
-            newSocket.off("system-message");
+            // newSocket.off("chat-history");
+            // newSocket.off("receive-message");
+            // newSocket.off("system-message");
             newSocket.disconnect();
         };
     }, [sessionId]);
@@ -58,17 +61,30 @@ export default function ChatPanel({ sessionId }) {
         <div className="flex flex-col h-full ">
             <div className="flex-1 overflow-y-auto">
                 {messages.map((msg, i) => (
-                    <div key={i} className={`p-2 ${msg.senderId === myId
-                        ? "text-right"
-                        : "text-left"
+                    <div key={i} className={`flex ${String(msg.senderId) === String(myId)
+                        // ? "text-right"
+                        // : "text-left"
+                        ? "justify-end"
+                        : "justify-start"
                         }`}>
-                        <span className="bg-gray-200 px-2 py-1 rounded">
+                        <div
+                            className={`px-3 py-2 rounded-lg max-w-[70%] ${String(msg.senderId) === String(myId)
+                                ? "bg-blue-500 text-white"
+                                : "bg-gray-200 text-black"
+                                }`}
+                        >
                             {msg.content}
-                        </span>
-                        {/* <b>{msg.senderId || "System"}:</b> {msg.content} */}
-                        <div style={{ fontSize: "10px" }}>
-                            {new Date(msg.createdAt).toLocaleTimeString()}
+                            <div className="text-[10px] opacity-70">
+                                {new Date(msg.createdAt).toLocaleTimeString()}
+                            </div>
                         </div>
+                        {/* <span className="bg-gray-200 px-2 py-1 rounded">
+                            {msg.content}
+                        </span> */}
+                        {/* <b>{msg.senderId || "System"}:</b> {msg.content} */}
+                        {/* <div style={{ fontSize: "10px" }}>
+                            {new Date(msg.createdAt).toLocaleTimeString()}
+                        </div> */}
                     </div>
                 ))}
             </div>
